cf40f4f5556981359720c3fcd2ccb7ed49d98bdb
Ivo Kosir <ivo.kosir93@gmail.com>
2016-01-31 20:21:53 +0100

    Added resources

--- src/FRP/Gearh.hs
+++ src/FRP/Gearh.hs
@@ -3,14 +3,20 @@ module FRP.Gearh
   , allways
   , sometimes
   , manytimes
-  , addIO
   , Output
   , output
   , quit
   , runGear
+  , ResourceStatus (..)
+  , ResourceRequest (..)
+  , Resource (resourceStatus, resourceRequest)
+  , createResource
+  , withResource
   ) where
 
 import Data.Maybe
+
+import Data.IORef
 
 newtype Input i = Input (IO [i])
 
@@ -33,37 +39,99 @@ sometimes io = Input $ fmap maybeToList io
 manytimes :: IO [a] -> Input a
 manytimes = Input
 
-addIO :: (a -> IO b) -> Input a -> Input b
-addIO io (Input i) = Input $ i >>= mapM io
-
-data Output
-  = Output (IO ())
-  | Quit
+data Output = Output { runOutput :: IO Bool }
 
 output :: IO () -> Output
-output = Output
+output io = Output $ io >> return False
 
 quit :: Output
-quit = Quit
+quit = Output $ return True
 
 instance Monoid Output where
-  mempty = Output $ return ()
-  mappend (Output oIO1) (Output oIO2) =
-    Output $ oIO1 >> oIO2
-  mappend o _ = o
+  mempty = Output $ return False
+  mappend (Output oIO1) (Output oIO2) = Output $ do
+    o1 <- oIO1
+    if o1
+      then return o1
+      else oIO2
 
 runGear
   :: Input (s -> s)
   -> (s -> Output)
   -> s
   -> IO s
-runGear (Input isIO) o initialState =
+runGear (Input isIO) outputF initialState =
   run initialState
  where
   run oldState = do
     is <- isIO
     let newState = foldl (\ state input -> input state) oldState is
 
-    case o newState of
-      Output oIO -> oIO >> run newState
-      Quit -> return newState
+    o <- runOutput (outputF newState)
+    if o
+      then return newState
+      else run newState
+
+
+data ResourceRequest
+  = Load
+  | Unload
+
+data ResourceStatus
+  = Loaded
+  -- | Loading
+  | Unloaded
+  -- | Unloading
+  deriving (Eq)
+
+data Resource a = Resource
+  { readResource :: IO (Maybe a)
+  , resourceRequest :: ResourceRequest -> Output
+  , resourceStatus :: Input ResourceStatus
+  }
+
+createResource
+  :: IO a
+  -> (a -> IO ())
+  -> IO (Resource a)
+createResource load unload = do
+  ref <- newIORef Nothing
+  statusRef <- newIORef Unloaded
+
+  let
+    resOutput req = output $ do
+      mA <- readIORef ref
+
+      case (req, mA) of
+
+        (Load, Nothing) -> do
+          a <- load
+          writeIORef ref (Just a)
+
+        (Unload, Just a) -> do
+          unload a
+          writeIORef ref Nothing
+
+        _ -> return ()
+
+    resInput = sometimes $ do
+      mA <- readIORef ref
+      oldStatus <- readIORef statusRef
+
+      let
+        newStatus = case mA of
+          Just _ -> Loaded
+          Nothing -> Unloaded
+
+      if newStatus == oldStatus
+        then return Nothing
+        else do
+          writeIORef statusRef newStatus
+          return $ Just newStatus
+
+  return $ Resource (readIORef ref) resOutput resInput
+
+withResource :: Resource a -> (b -> Output) -> (Maybe a -> b) -> Output
+withResource res out func = Output $ do
+  mA <- readResource res
+  runOutput $ out (func mA)
--- test/renderResources.hs
+++ test/renderResources.hs
@@ -4,7 +4,6 @@ import Graphics.Cogh
 
 import FRP.Gearh
 
-
 quitOutput :: Bool -> Output
 quitOutput True = quit
 quitOutput False = mempty
@@ -13,13 +12,22 @@ renderOutput :: Window -> Element -> Output
 renderOutput window =
   output . renderRoot window
 
+createImageResource
+  :: Window
+  -> String
+  -> IO (Resource Texture)
+createImageResource window imageLocation =
+  createResource createImage deleteImage
+ where
+  createImage = newTextureFromImage window imageLocation
+  deleteImage = deleteTexture
+
 eventInput :: Window -> Input Event
 eventInput = manytimes . getEvents
 
 data State = State
   { x :: Int
   , y :: Int
-  , clicks :: Int
   , finish :: Bool
   } deriving (Show)
 
@@ -27,16 +35,26 @@ updateEvent :: Event -> State -> State
 updateEvent Quit state = newState
  where
   newState = state { finish = True }
-updateEvent (MouseButton _ True _) state = newState
- where
-  newState = state { clicks = clicks state +1 }
 updateEvent (MousePosition mx my) state = newState
  where
   newState = state { x = mx, y = my }
 updateEvent _ state = state
 
-renderScene :: State -> Element
-renderScene state =
+renderScene :: State -> Maybe Texture -> Element
+renderScene state (Just texture) =
+  move centerX centerY $ group
+    [ move (-w2) (-h2) $ rectangle w2 h2 0xFF0000FF
+    , rectangle w2 h2 0x00FF00FF
+    , move (-110) 10 $ rectangle 100 100 0x0000FFFF
+    , move (-w4) (-h4) $ image w2 h2 texture
+    ]
+ where
+  (w, h) = (textureWidth texture, textureHeight texture)
+  (w2, h2) = (div w 2, div h 2)
+  (w4, h4) = (div w 4, div h 4)
+  centerX = x state
+  centerY = y state
+renderScene state Nothing =
   move centerX centerY $ group
     [ move (-w2) (-h2) $ rectangle w2 h2 0xFF0000FF
     , rectangle w2 h2 0x00FF00FF
@@ -51,19 +69,24 @@ renderScene state =
   centerY = y state
 
 initialState :: State
-initialState = State 0 0 0 False
+initialState = State 0 0 False
 
 main :: IO ()
 main = do
   (Just window) <- newWindow "Test"
 
+  res <- createImageResource window "test.png"
+
   let
-    i = fmap updateEvent (eventInput window)
+    i = mconcat
+      [ fmap updateEvent (eventInput window)
+      ]
 
     o = mconcat
       [ quitOutput . finish
-      , renderOutput window . renderScene
+      , withResource res (renderOutput window) . renderScene
+      , resourceRequest res . const Load
       ]
 
-  clicksReturn <- clicks <$> runGear i o initialState
-  putStrLn ("return value: " ++ show clicksReturn)
+  _ <- runGear i o initialState
+  return ()
